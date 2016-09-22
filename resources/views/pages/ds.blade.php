d127453ac8189b77108593c60bd02f31b9a5c365
nasehbadalov <nasehbadalov@gmail.com>
2016-09-22 17:03:10 +0400

    several pages are added

---
+++ resources/views/pages/ds.blade.php
@@ -0,0 +1,95 @@
+@extends('pages.layout')
+
+@section('content')
+  <section id="destek_olanlar">
+     <div class="container">
+        <div class="row">
+           <div class="col-md-6 col-sm-12">
+              <div class="row_padding">
+                 <div class="col-md-12">
+                    <div class="destek_img">
+                       <img class="img-responsive " src="image/1.jpg"/>
+                    </div>
+                 </div>
+                 <div class="col-md-12">
+                    <h2>Maşınımın təmirinə yardım</h2>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident et dolorum fuga.At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas </p>
+                    <h4>Ünvan: <b>28 may</b>&nbsp19/08/2016</h4>
+                    <a style="background-color: #6A1B9A;color:white" class="btn" href="{{URL('single')}}">Ətraflı</a>
+                 </div>
+              </div>
+           </div>
+           <div class="col-md-6 col-sm-12">
+              <div class="row_padding">
+                 <div class="col-md-12">
+                    <div class="destek_img">
+                       <img class="img-responsive " src="image/1.jpg"/>
+                    </div>
+                 </div>
+                 <div class="col-md-12">
+                    <h2>Maşınımın təmirinə yardım</h2>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident et dolorum fuga.At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas </p>
+                    <h4>Ünvan: <b>28 may</b>&nbsp19/08/2016</h4>
+                    <a style="background-color: #6A1B9A;color:white" class="btn" href="{{URL('single')}}">Ətraflı</a>
+                 </div>
+              </div>
+           </div>
+
+           <div class="col-md-6 col-sm-12">
+              <div class="row_padding">
+                 <div class="col-md-12">
+                    <div class="destek_img">
+                       <img class="img-responsive " src="image/1.jpg"/>
+                    </div>
+                 </div>
+                 <div class="col-md-12">
+                    <h2>Maşınımın təmirinə yardım</h2>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident et dolorum fuga.At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas </p>
+                    <h4>Ünvan: <b>28 may</b>&nbsp19/08/2016</h4>
+                    <a style="background-color: #6A1B9A;color:white" class="btn" href="{{URL('single')}}">Ətraflı</a>
+                 </div>
+              </div>
+           </div>
+
+           <div class="col-md-6 col-sm-12">
+              <div class="row_padding">
+                 <div class="col-md-12">
+                    <div class="destek_img">
+                       <img class="img-responsive " src="image/1.jpg"/>
+                    </div>
+                 </div>
+                 <div class="col-md-12">
+                    <h2>Maşınımın təmirinə yardım</h2>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident et dolorum fuga.At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas </p>
+                    <h4>Ünvan: <b>28 may</b>&nbsp19/08/2016</h4>
+                    <a style="background-color: #6A1B9A;color:white" class="btn" href="{{URL('single')}}">Ətraflı</a>
+                 </div>
+              </div>
+           </div>
+        </div>
+        <!-- PAGENATION -->
+        <nav aria-label="Page navigation">
+          <ul class="pagination pull-right">
+            <li class="page-item">
+              <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only">Previous</span>
+              </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">4</a></li>
+            <li class="page-item"><a class="page-link" href="#">5</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+     </div>
+  </section>
+@endsection
